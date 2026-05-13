d679d5e820bcfb1b05d939ea3738cce3b229fafc
ali alzein <aliialzein1@gmail.com>
2026-05-13 17:31:59 +0300

    Initial FastAPI setup

---
+++ app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {
+        "message": "Sentry Room Backend Running"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return {
+        "user_id": user_id,
+        "name": "Ali"
+    }
